50b268f10d8c1796b037b8bd5bc78e981954c9c5
lucas-m-braz <lucas_braz0@hotmail.com>
2022-02-15 13:48:32 -0300

    Exercício 1
    Exercício completo e funcionando

---
+++ Exercício 1 Variáveis/Exercício 1 Variáveis/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercício_1_Variáveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual seu nome?");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Qual sua idade?");
+            string idade = Console.ReadLine();
+
+            Console.WriteLine("Nome: " + nome + "\nano de nascimento: " + (DateTime.Now.Year - int.Parse(idade)));
+            Console.ReadLine();
+
+        }
+    }
+}
